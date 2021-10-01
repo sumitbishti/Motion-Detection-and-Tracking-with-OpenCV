17a547b5bf986f1abfb03ca259f4ee8b2c22f03c
beazybee <sumit7beazt@gmail.com>
2021-10-01 17:09:56 +0530

    added blurring.py

--- 9. blurring.py
+++ 9. blurring.py
@@ -2,22 +2,32 @@ import cv2 as cv
 
 img = cv.imread('photos/carr.jpg')
 cv.imshow('car', img)
+# canny = cv.Canny(img, 40, 50)
+# cv.imshow('canny', canny)
 
 # Averaging
-average = cv.blur(img, (7,7))
-cv.imshow('average', average)
+avg = cv.blur(img, (15,15))
+# cv.imshow('average', avg)
+# canny = cv.Canny(avg, 70, 80)
+# cv.imshow('canny', canny)
 
 # Gaussian Blur
-gauss = cv.GaussianBlur(img, (7,7), 0)
-cv.imshow('gauss', gauss)
+gauss = cv.GaussianBlur(img, (21,21),0)
+# cv.imshow('gauss', gauss)
+# canny = cv.Canny(gauss, 70, 80)
+# cv.imshow('canny', canny)
 
 # Median Blur
-median = cv.medianBlur(img, 7)
-cv.imshow('medain', median)
+median = cv.medianBlur(img, 11)
+# cv.imshow('medain', median)
+# canny = cv.Canny(median, 70, 80)
+# cv.imshow('canny', canny)
 
 # Bilateral Blur
-grayy = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-bilateral = cv.bilateralFilter(img, 20, 35, 35)
+# grayy = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+bilateral = cv.bilateralFilter(img, 7, 75, 75)
 cv.imshow('bilateral', bilateral)
+canny = cv.Canny(bilateral, 70, 80)
+cv.imshow('canny', canny)
 
 cv.waitKey(0)
